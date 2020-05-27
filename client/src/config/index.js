26e1187dfdace8bc790e79ca8538cbc81546d530
malikid <malikid.chang@gmail.com>
2020-05-27 02:37:02 +0200

    Added GA and HotJar.

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,1 +1,4 @@
 export const SERVER_HOST = process.env.SERVER_HOST || '';
+export const GA_CONTAINER_ID = process.env.REACT_APP_GA_CONTAINER_ID || '';
+export const HOTJAR_ID = process.env.REACT_APP_HOTJAR_ID || '';
+export const HOTJAR_VERSION = process.env.REACT_APP_HOTJAR_VERSION || '';
